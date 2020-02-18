b5eb358893a1b1872ecf9887318d516718394c46
Haruto Sato <k097dynasty@icloud.com>
2020-02-18 19:54:35 +0900

    index page modified and title color chnaged;qw:w

--- src/pages/index.md
+++ src/pages/index.md
@@ -2,8 +2,8 @@
 templateKey: index-page
 title: Sound Libre
 image: /img/palmtree.jpg
-heading: トロピカルハウスを中心にEDMアーティストの情報をお届けします。
-subheading: Support sustainable farming while enjoying a cup
+heading: 
+subheading: トロピカルハウスを中心にEDMアーティストの情報をお届けします。
 mainpitch:
   title: Why Kaldi
   description: >
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -41,7 +41,6 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -54,7 +53,6 @@ export const IndexPageTemplate = ({
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -74,26 +72,8 @@ export const IndexPageTemplate = ({
                   <div className="tile">
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
                 </div>
                 <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
